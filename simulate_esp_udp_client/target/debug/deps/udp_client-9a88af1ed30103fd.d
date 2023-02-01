0854cf9a5f0813aa5f26f2b62c7798c0018150ed
ronny002 <romankray@freenet.de>
2023-02-01 23:54:29 +0100

    Delete simulate_esp_udp_client/target directory

--- simulate_esp_udp_client/target/debug/deps/udp_client-9a88af1ed30103fd.d
+++
@@ -1,5 +0,0 @@
-/home/ronny/Documents/code/rust/esp_move_detect/simulate_esp_udp_client/target/debug/deps/udp_client-9a88af1ed30103fd: src/main.rs
-
-/home/ronny/Documents/code/rust/esp_move_detect/simulate_esp_udp_client/target/debug/deps/udp_client-9a88af1ed30103fd.d: src/main.rs
-
-src/main.rs:
--- simulate_esp_udp_client/target/debug/udp_client.d
+++
@@ -1,1 +0,0 @@
-/home/ronny/Documents/code/rust/esp_move_detect/simulate_esp_udp_client/target/debug/udp_client: /home/ronny/Documents/code/rust/esp_move_detect/simulate_esp_udp_client/src/main.rs
