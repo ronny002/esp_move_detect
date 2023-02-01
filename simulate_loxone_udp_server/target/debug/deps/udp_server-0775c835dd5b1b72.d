39477554b0b2cc553b102a91fbb0309579748c80
ronny002 <romankray@freenet.de>
2023-02-01 23:54:12 +0100

    Delete simulate_loxone_udp_server/target directory

--- simulate_loxone_udp_server/target/debug/deps/udp_server-0775c835dd5b1b72.d
+++
@@ -1,5 +0,0 @@
-/home/ronny/Documents/code/rust/esp_move_detect/simulate_loxone_udp_server/target/debug/deps/udp_server-0775c835dd5b1b72.rmeta: src/main.rs
-
-/home/ronny/Documents/code/rust/esp_move_detect/simulate_loxone_udp_server/target/debug/deps/udp_server-0775c835dd5b1b72.d: src/main.rs
-
-src/main.rs:
--- simulate_loxone_udp_server/target/debug/deps/udp_server-dd0e5f7c77296434.d
+++
@@ -1,5 +0,0 @@
-/home/ronny/Documents/code/rust/esp_move_detect/simulate_loxone_udp_server/target/debug/deps/udp_server-dd0e5f7c77296434: src/main.rs
-
-/home/ronny/Documents/code/rust/esp_move_detect/simulate_loxone_udp_server/target/debug/deps/udp_server-dd0e5f7c77296434.d: src/main.rs
-
-src/main.rs:
