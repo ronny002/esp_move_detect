4ceed7b2d04ad46a01523937cfdafb201d071640
ronny002 <romankray@freenet.de>
2023-02-05 17:38:54 +0100

    try http server in simulating mode

--- simulate_esp_udp_client/src/main.rs
+++ simulate_esp_udp_client/src/main.rs
@@ -1,21 +1,52 @@
-use std::{net::{UdpSocket, TcpStream}, thread, time::Duration, io::{Write, Read}};
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream, UdpSocket},
+    thread,
+    time::Duration,
+};
 
-fn main() {                                         //client
-    let socket = UdpSocket::bind("10.22.22.14:4002").expect("socket couldn't bind to address");
+fn main() {
+    //client
+    let socket = UdpSocket::bind("192.168.1.70:4002").expect("socket couldn't bind to address");
     socket
-        .connect("192.168.1.191:4003")   //server
+        .connect("192.168.1.38:4003") //server
         .expect("socket connect function failed");
 
-    let mut tcp_stream = TcpStream::connect("10.22.22.14:14002").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let handle = thread::spawn(move || {
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+            println!("Connection established!");
+            handle_connection(stream)
+        }
+    });
+
     println!("loop");
-    let mut tcp_buf = [0; 128];
     loop {
         // we are using thread::sleep here to make sure the watchdog isn't triggered
         thread::sleep(Duration::from_millis(1000));
+        println!("run");
 
         socket.send(&[5]).expect("couldn't send low message");
-        tcp_stream.write(&[7]).unwrap();
-        //tcp_stream.read(&mut tcp_buf).unwrap();
-        //println!("tcp: {:?}", tcp_buf);
     }
+    handle.join().unwrap();
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("{}", request_line);
+    let (status, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /ip HTTP/1.1" => ("HTTP/1.1 200 OK", "ip.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status, length, contents
+    );
+    stream.write_all(response.as_bytes()).unwrap();
 }
--- simulate_loxone_udp_server/src/main.rs
+++ simulate_loxone_udp_server/src/main.rs
@@ -1,25 +1,18 @@
-use std::io::{Write, Read};
-use std::net::{UdpSocket, TcpStream, TcpListener};
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, UdpSocket};
 use std::thread::sleep;
 use std::time::Duration;
-fn main() {                                         //server
-    let udp_socket = UdpSocket::bind("10.22.22.14:4003").unwrap();
-    println!("Successfully bind to 10.22.22.14:4003");
-    let mut tcp_stream = TcpListener::bind("10.22.22.14:14002").unwrap();
-    
+fn main() {
+    //server
+    let udp_socket = UdpSocket::bind("192.168.1.70:4003").unwrap();
+    println!("Successfully bind to 192.168.1.70:4003");
+
     let mut udp_buf = [0; 1];
-    let mut tcp_buf = String::from("");
 
     loop {
         sleep(Duration::from_millis(100));
-        
+
         udp_socket.recv_from(&mut udp_buf).unwrap();
         println!("udp: {:?}", udp_buf);
-        
-        for stream in tcp_stream.incoming(){
-            stream.unwrap().read_to_string(&mut tcp_buf).unwrap();
-        }
-        println!("tcp: {}", tcp_buf);
-
     }
 }
